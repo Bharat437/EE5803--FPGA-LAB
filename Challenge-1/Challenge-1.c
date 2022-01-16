6a2e871f02cfa275d0e0712d65b51dc589b02537
AVVARU BHARAT <70591588+Bharat437@users.noreply.github.com>
2022-01-16 11:30:31 +0530

    Update Challenge-1.c

--- Challenge-1/Challenge-1.c
+++ Challenge-1/Challenge-1.c
@@ -3,12 +3,12 @@
 int main()
 {
     bool x[8]={0,0,0,0,1,1,1,1},y[8]={0,0,1,1,0,0,1,1},z[8]={0,1,0,1,0,1,0,1},g[8]={0,0,1,0,1,1,0,1},sop,nand;
-    printf("X Y Z | ouput for SOP form | output using NAND gates\n");
+    printf("X Y Z | G | ouput for SOP form | output using NAND gates\n");
     for(int i=0;i<=7;++i)
     {   
         sop=((!x[i])&y[i]&(!z[i]))|(x[i]&(!y[i])&(!z[i]))|(x[i]&(!y[i])&z[i])|(x[i]&y[i]&z[i]);
         nand=!((!((!x[i])&y[i]&(!z[i])))&(!(x[i]&(!y[i])&(!z[i])))&(!(x[i]&(!y[i])&z[i]))&(!(x[i]&y[i]&z[i])));
-        printf("%d %d %d          %d                  %d\n", x[i],y[i],z[i],sop,nand);
+        printf("%d %d %d   %d          %d                  %d\n", x[i],y[i],z[i],g[i],sop,nand);
     }
     return 0;
 }
