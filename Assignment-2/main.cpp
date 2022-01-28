420e5fa284fe1ae7f53e7f8eddfa24aa37a7335e
AVVARU BHARAT <70591588+Bharat437@users.noreply.github.com>
2022-01-28 23:54:28 +0530

    Update main.cpp

--- Assignment-2/main.cpp
+++ Assignment-2/main.cpp
@@ -1,14 +1,32 @@
 #include <Arduino.h>
-int n=13;
+#define X 5
+#define Y 4
+#define Z 3
+#define W 2
+
+int x,y,z,w,F;
+
 void setup() {
-  pinMode(n,OUTPUT);
+  pinMode(LED_BUILTIN,OUTPUT);
+  pinMode(X,INPUT);
+  pinMode(Y,INPUT);
+  pinMode(Z,INPUT);
+  pinMode(W,INPUT);
   // put your setup code here, to run once:
 }
 
 void loop() {
-  digitalWrite(n,HIGH);
-  delay(1000);
-  digitalWrite(n,LOW);
-  delay(1000);
+  x=digitalRead(X);
+  y=digitalRead(Y);
+  z=digitalRead(Z);
+  w=digitalRead(W);
+  
+  F=!((!((!(x&&!y))&&z))&&w);
+  
+  if(F==1)
+          digitalWrite(LED_BUILTIN,HIGH);
+  else
+          digitalWrite(LED_BUILTIN,LOW);
+  
   // put your main code here, to run repeatedly:
 }
